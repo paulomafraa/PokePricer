f5b8f59a9ac2657466101303c5368957135e33ed
Paulo <paulomwatanabe02@gmail.com>
2025-12-14 16:46:40 -0300

    Commit inicial dados a preencher

---
+++ PokePricer2.0/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Drawing;
+
+namespace PokePricer2._0
+{
+    partial class Form1 : System.Windows.Forms.Form
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.CheckBox chkLembrar;
+        private System.Windows.Forms.Button btnExportar;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.chkLembrar = new System.Windows.Forms.CheckBox();
+            this.btnExportar = new System.Windows.Forms.Button();
+            this.executarPrice = new System.Windows.Forms.CheckBox();
+            this.btnPrecificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Location = new System.Drawing.Point(30, 30);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(36, 13);
+            this.lblLogin.TabIndex = 0;
+            this.lblLogin.Text = "Login:";
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(80, 27);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(200, 20);
+            this.txtLogin.TabIndex = 1;
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Location = new System.Drawing.Point(30, 70);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(41, 13);
+            this.lblSenha.TabIndex = 2;
+            this.lblSenha.Text = "Senha:";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(80, 67);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(200, 20);
+            this.txtSenha.TabIndex = 3;
+            this.txtSenha.UseSystemPasswordChar = true;
+            // 
+            // chkLembrar
+            // 
+            this.chkLembrar.AutoSize = true;
+            this.chkLembrar.Location = new System.Drawing.Point(33, 120);
+            this.chkLembrar.Name = "chkLembrar";
+            this.chkLembrar.Size = new System.Drawing.Size(130, 17);
+            this.chkLembrar.TabIndex = 4;
+            this.chkLembrar.Text = "Lembrar login e senha";
+            // 
+            // btnExportar
+            // 
+            this.btnExportar.Location = new System.Drawing.Point(177, 163);
+            this.btnExportar.Name = "btnExportar";
+            this.btnExportar.Size = new System.Drawing.Size(103, 30);
+            this.btnExportar.TabIndex = 5;
+            this.btnExportar.Text = "Exportar Cartas";
+            this.btnExportar.UseVisualStyleBackColor = true;
+            this.btnExportar.Click += new System.EventHandler(this.btnExportar_Click);
+            // 
+            // executarPrice
+            // 
+            this.executarPrice.AutoSize = true;
+            this.executarPrice.Location = new System.Drawing.Point(177, 120);
+            this.executarPrice.Name = "executarPrice";
+            this.executarPrice.Size = new System.Drawing.Size(103, 17);
+            this.executarPrice.TabIndex = 6;
+            this.executarPrice.Text = "Converter Preço";
+            this.executarPrice.UseVisualStyleBackColor = true;
+            // 
+            // btnPrecificar
+            // 
+            this.btnPrecificar.Location = new System.Drawing.Point(33, 163);
+            this.btnPrecificar.Name = "btnPrecificar";
+            this.btnPrecificar.Size = new System.Drawing.Size(103, 30);
+            this.btnPrecificar.TabIndex = 7;
+            this.btnPrecificar.Text = "Precificar";
+            this.btnPrecificar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 222);
+            this.Controls.Add(this.btnPrecificar);
+            this.Controls.Add(this.executarPrice);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.chkLembrar);
+            this.Controls.Add(this.btnExportar);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PokePricer - Exportador";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox executarPrice;
+        private System.Windows.Forms.Button btnPrecificar;
+    }
+}
---
+++ PokePricer2.0/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using Newtonsoft.Json;
+
+namespace PokePricer2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtLogin.Text = "";
+            txtSenha.Text = "";
+            btnPrecificar.Click += btnPrecificar_Click;
+        }
+
+        private void btnExportar_Click(object sender, EventArgs e)
+        {
+            string jsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "cartas.json");
+            string precosPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "precos_pokemon.json");
+
+            var options = new ChromeOptions();
+            var service = ChromeDriverService.CreateDefaultService();
+
+            var cartasExtraidas = new List<CartaPokemon>();
+
+            using (var driver = new ChromeDriver(service, options))
+            {
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+
+                driver.Navigate().GoToUrl("https://www.tcgcollector.com/account/sign-in");
+
+                var inputUser = wait.Until(d => d.FindElement(By.CssSelector("input#sign-in-email-address")));
+                inputUser.Clear();
+                inputUser.SendKeys(txtLogin.Text);
+
+                var inputPass = driver.FindElement(By.CssSelector("input#sign-in-password"));
+                inputPass.Clear();
+                inputPass.SendKeys(txtSenha.Text);
+
+                var btnSubmit = driver.FindElement(By.XPath("//button[@type='submit']"));
+                btnSubmit.Click();
+
+                wait.Until(d => d.Url.Contains("dashboard"));
+
+                string baseUrl = "https://www.tcgcollector.com/cards?cardSource=inCardCollection&releaseDateOrder=newToOld&displayAs=images&cardsPerPage=120";
+                driver.Navigate().GoToUrl(baseUrl);
+                System.Threading.Thread.Sleep(500);
+
+                var pageItems = driver.FindElements(By.CssSelector("li.pagination-item"));
+                var pageNumbers = new List<int>();
+                foreach (var item in pageItems)
+                {
+                    try
+                    {
+                        var link = item.FindElement(By.CssSelector("a.pagination-link"));
+                        int num = int.Parse(link.Text.Trim());
+                        pageNumbers.Add(num);
+                    }
+                    catch { }
+                }
+                int totalPages = pageNumbers.Count > 0 ? Math.Max(1, pageNumbers[pageNumbers.Count - 1]) : 1;
+
+                for (int pageNum = 1; pageNum <= totalPages; pageNum++)
+                {
+                    string url = pageNum > 1 ? $"{baseUrl}&page={pageNum}" : baseUrl;
+                    driver.Navigate().GoToUrl(url);
+
+                    wait.Until(d => d.FindElements(By.CssSelector("div.card-image-grid-item")).Count > 0);
+
+                    var cards = driver.FindElements(By.CssSelector("div.card-image-grid-item"));
+                    foreach (var card in cards)
+                    {
+                        try
+                        {
+                            var linkElement = card.FindElement(By.CssSelector("a.card-image-grid-item-link"));
+                            string title = linkElement.GetAttribute("title");
+                            string cardId = card.GetAttribute("data-card-id");
+                            string cardUrl = linkElement.GetAttribute("href");
+
+                            string name, set;
+                            if (title.Contains("(") && title.Contains(")"))
+                            {
+                                name = title.Split('(')[0].Trim();
+                                string conteudoParenteses = title.Split('(')[1].Split(')')[0].Trim();
+                                var partes = conteudoParenteses.Split(' ');
+                                string numero = partes[partes.Length - 1];
+
+                                string setFormatado = numero;
+                                if (numero.Contains("/"))
+                                {
+                                    var splitSet = numero.Split('/');
+                                    if (splitSet.Length == 2)
+                                        setFormatado = $"{splitSet[0].Trim()}/{splitSet[1].Trim()}";
+                                }
+                                set = setFormatado;
+                            }
+                            else
+                            {
+                                name = title.Trim();
+                                set = "Desconhecido";
+                            }
+
+                            cartasExtraidas.Add(new CartaPokemon
+                            {
+                                Nome = name,
+                                Set = set,
+                                CardId = cardId,
+                                Url = cardUrl
+                            });
+                        }
+                        catch { }
+                    }
+                }
+            }
+
+            List<CartaPokemon> cartasSalvas = new List<CartaPokemon>();
+            if (File.Exists(jsonPath))
+            {
+                string jsonExistente = File.ReadAllText(jsonPath);
+                cartasSalvas = JsonConvert.DeserializeObject<List<CartaPokemon>>(jsonExistente) ?? new List<CartaPokemon>();
+            }
+
+            foreach (var carta in cartasExtraidas)
+            {
+                if (!cartasSalvas.Exists(c => c.CardId == carta.CardId))
+                    cartasSalvas.Add(carta);
+            }
+            File.WriteAllText(jsonPath, JsonConvert.SerializeObject(cartasSalvas, Formatting.Indented));
+
+            if (!executarPrice.Checked)
+            {
+                MessageBox.Show("Cartas exportadas para cartas.json com sucesso!");
+            }
+
+            if (executarPrice.Checked)
+            {
+                ConverterPrecos();
+            }
+        }
+
+        private void ConverterPrecos()
+        {
+            string cartasPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "cartas.json");
+            string precosPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "precos_pokemon.json");
+
+            if (!File.Exists(cartasPath))
+            {
+                MessageBox.Show("Arquivo cartas.json não encontrado!");
+                return;
+            }
+
+            var cartas = JsonConvert.DeserializeObject<List<CartaPokemon>>(File.ReadAllText(cartasPath));
+            var precos = new List<CartaPreco>();
+
+            var options = new ChromeOptions();
+            var service = ChromeDriverService.CreateDefaultService();
+
+            using (var driver = new ChromeDriver(service, options))
+            {
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+
+                foreach (var carta in cartas)
+                {
+                    try
+                    {
+                        string numeroCarta = carta.Set;
+
+                        string searchQuery = Uri.EscapeDataString($"{carta.Nome} ({numeroCarta})");
+                        string url = $"https://www.ligapokemon.com.br/?view=cards/search&card={searchQuery}";
+
+                        driver.Navigate().GoToUrl(url);
+                        wait.Until(d => d.FindElements(By.CssSelector("div.card-image-grid-item")).Count > 0);
+
+                        var cardBoxes = driver.FindElements(By.CssSelector("#mtg-cards .box.p25"));
+
+                        bool clicou = false;
+                        foreach (var box in cardBoxes)
+                        {
+                            var nomeEl = box.FindElement(By.CssSelector(".mtg-name a"));
+                            var nome = nomeEl.Text.Trim();
+
+                            var edicaoEl = box.FindElement(By.CssSelector(".edition-name"));
+                            var edicao = edicaoEl.Text.Trim();
+
+                            if (nome.Equals(carta.Nome, StringComparison.OrdinalIgnoreCase) && !edicao.Contains("JP"))
+                            {
+                                nomeEl.Click();
+                                clicou = true;
+                                break;
+                            }
+                        }
+
+                        if (!clicou && cardBoxes.Count > 1)
+                        {
+                            MessageBox.Show($"{carta.Nome} PT/EN não encontrado nos resultados!");
+                        }
+
+                        var priceElements = driver.FindElements(By.CssSelector("div.new-price.price-with-image, .new-price, .price"));
+                        decimal menorPreco = -1;
+                        string condicaoMenorPreco = null;
+                        foreach (var el in priceElements)
+                        {
+                            string priceText = el.Text.Replace("R$", "").Replace(".", "").Replace(",", ".").Trim();
+                            string condicao = null;
+                            try
+                            {
+                                var ancestor = el.FindElement(By.XPath("ancestor::div[contains(@class,'quantity-and-price')]"));
+                                var qualityDiv = ancestor.FindElement(By.CssSelector("div.quality"));
+                                condicao = qualityDiv.GetAttribute("title")?.Trim() ?? qualityDiv.Text.Trim();
+                            }
+                            catch
+                            {
+                                condicao = "Desconhecida";
+                            }
+
+                            if (decimal.TryParse(priceText, out decimal preco))
+                            {
+                                if (menorPreco == -1 || preco < menorPreco)
+                                {
+                                    menorPreco = preco;
+                                    condicaoMenorPreco = condicao;
+                                }
+                            }
+                        }
+
+                        precos.Add(new CartaPreco
+                        {
+                            Nome = carta.Nome,
+                            Set = carta.Set,
+                            CardId = carta.CardId,
+                            Url = carta.Url,
+                            Preco = menorPreco > 0 ? menorPreco : (decimal?)null,
+                            Condicao = condicaoMenorPreco
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                        precos.Add(new CartaPreco
+                        {
+                            Nome = carta.Nome,
+                            Set = carta.Set,
+                            CardId = carta.CardId,
+                            Url = carta.Url,
+                            Preco = null
+                        });
+                    }
+                }
+            }
+
+            File.WriteAllText(precosPath, JsonConvert.SerializeObject(precos, Formatting.Indented));
+            MessageBox.Show("Preços exportados para precos_pokemon.json com sucesso!");
+        }
+
+        private void btnPrecificar_Click(object sender, EventArgs e)
+        {
+            ConverterPrecos();
+        }
+    }
+
+    public class CartaPokemon
+    {
+        public string Nome { get; set; }
+        public string Set { get; set; }
+        public string CardId { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class CartaPreco
+    {
+        public string Nome { get; set; }
+        public string Set { get; set; }
+        public string CardId { get; set; }
+        public string Url { get; set; }
+        public decimal? Preco { get; set; }
+        public string Condicao { get; set; }
+    }
+}
